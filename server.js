313a9ea9043a0fe0629684a32aa72d1bf5c42abc
sayed94h <skazimi951@yahoo.com>
2021-03-18 08:52:43 +0100

    fixed wrong url

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 "use strict";
 
-const { required } = require("joi");
-
 const express = require("express");
 const path = require("path");
-const bodyParser = require("body-parser");
 const logger = require("./logger");
 const config = require("./config");
 const cors = require("cors");
@@ -12,8 +9,8 @@ const api = require("./api");
 const app = express();
 app.use(logger);
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/", express.static(path.join(__dirname, "public")));
 // in case if there is any error
 app.use(function (err, req, res, next) {
