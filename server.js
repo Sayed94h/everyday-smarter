47f62dbdcef0d0eb19304c4ab58466a90613dad3
sayed94h <skazimi951@yahoo.com>
2021-04-15 22:44:29 +0200

    removed videos

--- server.js
+++ server.js
@@ -12,6 +12,32 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/home", express.static(path.join(__dirname, "public")));
+app.use(
+	"/projects",
+	express.static(path.join(__dirname, "public/projects.html"))
+);
+app.use(
+	"/services",
+	express.static(path.join(__dirname, "public/services.html"))
+);
+app.use("/about", express.static(path.join(__dirname, "public/about.html")));
+app.use(
+	"/contacts",
+	express.static(path.join(__dirname, "public/contacts.html"))
+);
+app.use(
+	"/feedback",
+	express.static(path.join(__dirname, "public/feedback.html"))
+);
+app.use(
+	"/repositories",
+	express.static(path.join(__dirname, "public/repositories.html"))
+);
+app.use(
+	"/messages_page",
+	express.static(path.join(__dirname, "public/messages_page.html"))
+);
 // in case if there is any error
 app.use(function (err, req, res, next) {
 	if (err) {
@@ -19,6 +45,7 @@ app.use(function (err, req, res, next) {
 		res.status(500).end();
 		return;
 	}
+	next();
 });
 app.use("/api", api);
 // open the server
